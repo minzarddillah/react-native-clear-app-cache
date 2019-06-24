b6c9f4b27ff9b6dd02bfc5b1afe65ceeaec7ec03
Minzard <minzard@myboost.id>
2019-06-24 10:10:59 +0700

    migration androidX

--- android/src/main/java/com/learnta/clear/ClearCacheModule.java
+++ android/src/main/java/com/learnta/clear/ClearCacheModule.java
@@ -1,7 +1,7 @@
 package com.learnta.clear;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
